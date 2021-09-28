3807b1cd104b9df68dd41206a8d742389266479f
kundan kumar <kundankumar5037@gmail.com>
2021-09-28 15:53:01 +0530

    My first commit

--- app.js
+++ app.js
@@ -25,8 +25,9 @@ const store = new MongoDBStore({
   collection: 'sessions'
 });
 const csrfProtection = csrf();
-const privateKey = fs.readFileSync('server.key');
-const certificate = fs.readFileSync('server.cert');
+
+//const privateKey = fs.readFileSync('server.key');
+//const certificate = fs.readFileSync('server.cert');
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -124,7 +125,10 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    https.createServer({key:privateKey, cert:certificate}, app).listen(process.env.PORT || 3000);
+   // https.
+   // createServer({key:privateKey, cert:certificate}, app).
+   // listen(process.env.PORT || 3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch(err => {
     console.log(err);
